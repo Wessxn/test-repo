7510a1b2492af7b7c42adf9165a337e2ea6d53a3
Wessxn <parkermarcus282@gmail.com>
2022-06-25 16:49:31 -0400

    commit on different branch

--- files/src/main.rs
+++ files/src/main.rs
@@ -1,3 +1,24 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // --snip--
+
+    let user1 = User {
+        email: String::from("someone@dwexample.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    let user2 = User {
+        active: user1.active,
+        username: user1.username,
+        email: String::from("another@exampsle.com"),
+        sign_in_count: user1.sign_in_count,
+    };
 }
